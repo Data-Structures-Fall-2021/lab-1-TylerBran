af8ed4383df95279fef63bc9572b5bb0bd518490
TylerBran <90166780+TylerBran@users.noreply.github.com>
2021-09-11 19:50:42 -0400

    Update TaylorSeries.java

--- src/main/java/TaylorSeries.java
+++ src/main/java/TaylorSeries.java
@@ -1,5 +1,26 @@
 public class TaylorSeries {
-
-    // YOUR CODE GOES HERE
+    public double approximateExp(int exponent, int NumIterations){
+        double approx=0;
+        int Iterations=0;
+        while(Iterations<=NumIterations){
+            approx=approx+(pow(exponent,Iterations)/factorial(Iterations));
+            Iterations++;
+        }
+        return approx;
+    }
+    public double factorial(int n){
+        if(n==0){
+            return 1;
+        } else{
+            return(n*factorial(n-1));
+        }
+    }
+    public double pow(int x, int n){
+        if(n==0) {
+            return 1;
+        } else{
+            return(x*pow(x,n-1));
+        }
+    }
 
 }
